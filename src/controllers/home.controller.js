fad887c903d05e09d807fd38bf6ef5518a78210d
Edi Ubaedillah <ubaedillahedi@gmail.com>
2023-02-11 18:01:08 +0700

    Show list data from mongodb in home page

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,3 +1,9 @@
-exports.index = (req, res) => {
-    res.render('index')
+const Article = require('../models/article.model')
+
+exports.index = async (req, res) => {
+    let articles = await Article.find()
+    // console.log(articles)
+    res.render('index', {
+        data: articles
+    })
 }
