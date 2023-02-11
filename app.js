06b0498d5bad82792d3c97149b3a75760e27a429
Edi Ubaedillah <ubaedillahedi@gmail.com>
2023-02-11 14:46:08 +0700

    templating views

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express()
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+// untuk url static akan diarahkan kesitu
+app.use(express.static('public'))
+// setiap ada url dengan kata css,js atau img akan diarahkan sesuai dengan foldernya
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
 require('./src/routes/home.routes')(app)
 
 const PORT = 3000
